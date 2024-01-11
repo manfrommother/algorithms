712f13d06fa5fb801d1d8f6c908c5d998a3ceafb
manfrommother <mr.lipova@list.ru>
2024-01-11 13:02:04 +0300

    simple binary search

---
+++ binart_search.py
@@ -0,0 +1,18 @@
+def binary_search(list, item):
+    low = 0
+    hight = len(list) - 1
+
+    while low <= hight:
+        mid = int((hight + low) / 2)
+        guess = list[mid]
+        if guess == item:
+            return mid 
+        elif guess > item:
+            hight = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+lst = [1, 2, 6, 9]
+print(binary_search(lst, 6))
